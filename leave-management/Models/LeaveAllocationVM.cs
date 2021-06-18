f8c7fe84b8a563e04e4228804c07a5d944dc685d
Iwu Chinonso <irnon3y@gmail.com>
2021-06-18 16:13:32 +0100

    Add project files.

---
+++ leave-management/Models/LeaveAllocationVM.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class LeaveAllocationVM
+    {
+        public int ID { get; set; }
+        [Required]
+        public int NumberofDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        public EmployeeVM Employee { get; set; }
+        public string EmployeeId { get; set; }
+        public DetailsLeaveTypeVM LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public IEnumerable <SelectList> Employees { get; set; }
+        public IEnumerable <SelectList> LeaveTypes { get; set; }
+    }
+}
---
+++ leave-management/Repository/EmployeeRepository.cs
@@ -0,0 +1,60 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmployeeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(Employee entity)
+        {
+            _db.Employees.Add(entity);
+            
+            return Save();
+        }
+
+        public bool Delete(Employee entity)
+        {
+            _db.Employees.Remove(entity);
+
+            return Save();
+        }
+
+        public ICollection<Employee> FindAll()
+        {
+            var AllEmployees = _db.Employees.ToList();
+
+            return AllEmployees;
+        }
+
+        public Employee FindById(int id)
+        {
+            var employee = _db.Employees.Find(id);
+
+            return employee;
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+
+            return changes > 0;
+        }
+
+        public bool Update(Employee entity)
+        {
+            _db.Employees.Update(entity);
+
+            return Save();
+        }
+    }
+}
