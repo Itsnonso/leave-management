db8fabe9d009c68c73ff6e1b58bc2515cfe6c15c
Iwu Chinonso <irnon3y@gmail.com>
2021-06-20 17:28:33 +0100

    Created User Authentication and Authorization with Default Roles. Administrator and Employee

---
+++ leave-management/SeedData.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> usermanager, RoleManager<IdentityRole> rolemanager)
+        {
+            SeedRoles(rolemanager);
+            SeedUsers(usermanager);
+        }
+        private static void SeedUsers(UserManager<IdentityUser> usermanager)
+        {
+            if (usermanager.FindByNameAsync("admin").Result == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = "admin",
+                    Email = "admin@localhost.com"
+
+                };
+                var result = usermanager.CreateAsync(user, "Password@1").Result;
+
+                if (result.Succeeded)
+                {
+                    usermanager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+
+        }
+        private static void SeedRoles(RoleManager<IdentityRole> rolemanager)
+        {
+            if (!rolemanager.RoleExistsAsync("Administrator").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Administrator",
+
+                };
+               var result = rolemanager.CreateAsync(role).Result;
+            }
+
+            if (!rolemanager.RoleExistsAsync("Employee").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Employee",
+
+                };
+                var result = rolemanager.CreateAsync(role).Result;
+            }
+        }
+    }
+}
