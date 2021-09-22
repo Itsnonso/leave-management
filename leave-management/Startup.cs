93ef0943e740ad40746fab316cebe0ddb37f1732
Iwu Chinonso <irnon3y@gmail.com>
2021-09-22 14:42:58 +0100

    Test

--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace leave_management
 {
@@ -24,7 +25,10 @@ namespace leave_management
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
 
             //Add references for Repository and Contracts to Startup File 
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
